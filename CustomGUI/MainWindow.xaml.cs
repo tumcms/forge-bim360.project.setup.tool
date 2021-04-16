66e2fadeb3fb4dbe996b30181908a7548b9ebbbd
h_stefan <stefan.1995.huber@tum.de>
2021-04-16 12:59:30 +0200

    GUI Prototype

---
+++ CustomGUI/MainWindow.xaml.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CustomGUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //Classes for Datagrids
+        //one instand of a class contains data of a row in the CSV file
+        UserData test = new UserData();
+        
+
+        public MainWindow()
+        {
+            //TODO: configfile handle -> on start read out configfile all id and set them
+            InitializeComponent();
+            List<UserData> usermanag = new List<UserData>();
+            usermanag.Add(new UserData { _projectname = "Test1",_projecttype="mehrTest"  });
+            data.Items.Add(usermanag[0]);
+
+        }
+
+
+
+        //Event Handling
+
+
+        //Tab Config
+        private void saveconf_Click(object sender, RoutedEventArgs e)
+        {
+            /* TODO: save config data in the Texboxes into the Config File 
+               or create new if not exists            
+             */
+        }
+
+        //Remove input if use click on it
+        private void clientid_GotKeyboardFocus(object sender, RoutedEventArgs e)
+        {
+            if (clientid.Text.Equals("Forge_Client_ID"))
+            {
+                clientid.Text = "";
+            }
+        }
+        private void bimid_GotKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            if (bimid.Text.Equals("Forge_Bim_Account_ID"))
+            {
+                bimid.Text = "";
+            }
+        }
+        private void clientsecret_GotKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            if (clientsecret.Text.Equals("Forge_Client_Secret"))
+            {
+                clientsecret.Text = "";
+            }
+        }
+
+
+
+        //UserManagement
+
+
+        //CSVToolkit
+
+        private void buttonimport_Click(object sender, RoutedEventArgs e)
+        {
+            if (!File.Exists(csvpath.Text)){
+                statusbar.Text = "Import Failed! File not found!";
+            }
+
+        }
+
+        //Remove input if use click on it
+        private void csvpath_GotKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+
+            if (csvpath.Text.Equals("C:\\Users\\Example1\\example.csv"))
+            {
+                csvpath.Text = "";
+            }
+        }
+
+        private void buttonexport_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ CustomGUI/UserData.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomGUI
+{
+    public class UserData
+    {
+        public string _projectname { get; set; }
+        public string _projecttype { get; set; }
+        public string _root_folder { get; set; }
+
+        //Liste für Level erstellen
+        public string _level_1 { get; set; }
+        public string _level_2 { get; set; }
+        public string _level_3 { get; set; }
+        public string _permission { get; set; }
+        public string _role_permission { get; set; }
+        public string _user_email { get; set; }
+        public string _industry_role { get; set; }
+        public string _company { get; set; }
+        public string _company_trade { get; set; }
+        public string _local_folder { get; set; }
+
+        /* // not sure if needed
+        public Datalist(string projectname,string projecttype,string root_folder,
+            string level_1,string level_2,string level_3, string permission,
+            string role_permission,string user_email,string industry_role, 
+            string company,string company_trade,string local_folder)
+        {
+            _projectname = projectname;
+            _projecttype = projecttype;
+            _root_folder = root_folder;
+            _level_1 = level_1;
+            _level_2 = level_2;
+            _level_3 = level_3;
+            _permission = permission;
+            _role_permission = role_permission;
+            _user_email = user_email;
+            _industry_role = industry_role;
+            _company = company;
+            _company_trade = company_trade;
+            _local_folder = local_folder;
+
+        }
+        public Datalist() 
+        { }
+        */
+
+            /// <summary>
+            /// Maps CSV to UserData input
+            /// </summary>
+        public void mapcsvtoinput() { }
+
+    }
+
+}
