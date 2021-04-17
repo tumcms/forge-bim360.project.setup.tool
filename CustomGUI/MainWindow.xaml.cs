2e7ae7af93660a0a90ce296fae0e022b3b986931
h_stefan <stefan.1995.huber@tum.de>
2021-04-17 10:20:45 +0200

    Update Config Input -> now Config will be saved in an txt and is importet on start

--- CustomGUI/MainWindow.xaml.cs
+++ CustomGUI/MainWindow.xaml.cs
@@ -29,13 +29,33 @@ namespace CustomGUI
 
         //Classes for Datagrids
         //one instand of a class contains data of a row in the CSV file
+        //Global Data
         List<UserData> usermanag = new List<UserData>();
-
+        StackPanel save_conf;
+        string conf_clientid ;
+        string conf_clientsecret;
+        string conf_bimid;
+        string path_file = ".\\Config\\config.txt";
 
         public MainWindow()
         {
             //TODO: configfile handle -> on start read out configfile all id and set them
             InitializeComponent();
+            //Read Config out of Configfile
+            if (File.Exists(path_file))
+            {
+                using(FileStream fs= File.OpenRead(path_file))
+                {
+                    using(StreamReader reader = new StreamReader(fs))
+                    {
+                        conf_clientid=reader.ReadLine();
+                        conf_clientsecret = reader.ReadLine();
+                        conf_bimid = reader.ReadLine();
+                    }
+                }
+            }
+
+
             usermanag.Add(new UserData());
             List<string> temp = new List<string>();
             usermanag[0]._sub_folder = temp;
@@ -50,8 +70,10 @@ namespace CustomGUI
 
         //Event Handling
 
-
+        
         //Tab Config
+
+        //needs to be removed until (*)
         private void saveconf_Click(object sender, RoutedEventArgs e)
         {
             /* TODO: save config data in the Texboxes into the Config File 
@@ -82,7 +104,7 @@ namespace CustomGUI
             }
         }
 
-
+        // Here (*)
 
         //UserManagement
 
@@ -136,18 +158,116 @@ namespace CustomGUI
         {
             var window = new Window();
             window.Owner = this;
-            //Add the right Config 
-            var stackPanel = new StackPanel { Orientation = Orientation.Vertical };
-            stackPanel.Children.Add(new TextBlock { HorizontalAlignment = HorizontalAlignment.Left,
-                           Margin = new Thickness(40,40,0,0),
-                           Text = "Forge_Client_ID",
-                           VerticalAlignment = VerticalAlignment.Top,
-                           FontSize = 16
+            //Add config input boxes
+            save_conf = new StackPanel { Orientation = Orientation.Vertical };
+            save_conf.Children.Add(new TextBlock 
+            { 
+                HorizontalAlignment = HorizontalAlignment.Left,
+                Margin = new Thickness(40,40,0,0),
+                Text = "Forge_Client_ID",
+                VerticalAlignment = VerticalAlignment.Top,
+                FontSize = 16,
+                FontWeight=FontWeights.Bold
             });
-            stackPanel.Children.Add(new TextBox {  });
-            window.Content = stackPanel;
+            save_conf.Children.Add(new TextBox 
+            { 
+                Name = "Forge_Client_ID",
+                Text = conf_clientid,
+                HorizontalAlignment = HorizontalAlignment.Left,
+                Margin = new Thickness(200,40,0,0),
+                VerticalAlignment = VerticalAlignment.Top,
+                Width = 300,
+            });
+
+            save_conf.Children.Add(new TextBlock
+            {
+                HorizontalAlignment = HorizontalAlignment.Left,
+                Margin = new Thickness(40, 80, 0, 0),
+                Text = "Forge_Client_Secret",
+                VerticalAlignment = VerticalAlignment.Top,
+                FontSize = 16,
+                FontWeight = FontWeights.Bold
+            });
+            save_conf.Children.Add(new TextBox
+            {
+                Name = "clientsecret",
+                Text = conf_clientsecret,
+                HorizontalAlignment = HorizontalAlignment.Left,
+                Margin = new Thickness(200, 80, 0, 0),
+                VerticalAlignment = VerticalAlignment.Top,
+                Width = 300,
+            });
+
+            save_conf.Children.Add(new TextBlock
+            {
+                HorizontalAlignment = HorizontalAlignment.Left,
+                Margin = new Thickness(40, 120, 0, 0),
+                Text = "Forge_Bim_Account_ID",
+                VerticalAlignment = VerticalAlignment.Top,
+                FontSize = 16,
+                FontWeight = FontWeights.Bold
+            });
+            save_conf.Children.Add(new TextBox
+            {
+                Name = "bimid",
+                Text = conf_bimid,
+                HorizontalAlignment = HorizontalAlignment.Left,
+                Margin = new Thickness(200, 120, 0, 0),
+                VerticalAlignment = VerticalAlignment.Top,
+                Width = 300,
+                //add on change event handle
+            }) ;
+
+            save_conf.Children.Add(new Button
+            {
+                Content="Save Config",
+                HorizontalAlignment= HorizontalAlignment.Left,
+                Margin = new Thickness(604,159,0,0),
+                VerticalAlignment= VerticalAlignment.Top,
+            });
+
+            window.Closed += Window_Closed;
+            window.Content = save_conf;
             window.ShowDialog();
-            
+
+
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+
+            string path_folder = ".\\Config";
+
+            //quick and dirty solution -> mainly for testing
+            var temp = save_conf.Children.OfType<TextBox>().ToList();
+            conf_clientid = temp[0].Text;
+            conf_clientsecret = temp[1].Text;
+            conf_bimid = temp[2].Text;
+
+
+
+            //Create Config Folder
+            if (!Directory.Exists(path_folder))
+            {
+                Directory.CreateDirectory(path_folder);
+            }
+            //Check if Config already exists
+            if (File.Exists(path_file))
+            {
+                File.Delete(path_file);
+            }
+
+            //Write config into txt
+            using(FileStream fs = File.Create(path_file))
+            {
+                using(var sr= new StreamWriter(fs))
+                {
+                    sr.WriteLine(conf_clientid);
+                    sr.WriteLine(conf_clientsecret);
+                    sr.WriteLine(conf_bimid);
+                }
+            }
+
         }
     }
 }
